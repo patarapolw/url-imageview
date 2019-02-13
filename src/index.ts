e32b1c15042d1bef0a806b295f2b36b12f91861b
patarapolw <patarapolw@gmail.com>
2019-02-13 13:08:35 +0700

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import Viewer from "viewerjs";
 import $ from "jquery";
 
-import urlList from "./urlList.txt";
+import urlList from "./user/urlList.txt";
 import "viewerjs/dist/viewer.css";
 import "./index.css";
 
 (() => {
     const mainArea = document.getElementById("App") as HTMLDivElement;
     const $mainArea = $(mainArea);
-    urlList.trim().split("\n").forEach((url: any) => {
+    shuffle(urlList.trim().split("\n")).forEach((url: any) => {
         const $img = $(`
             <div class="thumbnail-wrapper">
                 <img class="thumbnail uncaptioned" src="${url}" alt="${
@@ -19,3 +19,11 @@ import "./index.css";
 
     const gallery = new Viewer(mainArea);
 })();
+
+function shuffle(a: any[]) {
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+}
